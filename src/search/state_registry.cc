56b2f862ea40d47e79243101cd01763bb5482b32
Oliver Harold <oliverhsj@hotmail.dk>
2025-04-30 06:43:27 +0200

    bug fix

--- src/search/state_registry.cc
+++ src/search/state_registry.cc
@@ -36,7 +36,7 @@ void prepare_file(TaskProxy task_proxy, int num_bins) {
         }
         outfile << '\n';
         outfile << -1;
-        outfile << -1;
+        outfile << ',' << -1;
         for (std::size_t i = 0; i < num_bins; ++i) {
             outfile << ',' << -1;
         }
