eb30c2e69bf8f356dbd7f0b3117331da7d605bc4
Dominik Drexler <drexlerd93@gmail.com>
2025-10-15 11:47:39 +0200

    use PackedStateBin as tree node value

--- src/search/state_registries/tree_packed_state_registry.h
+++ src/search/state_registries/tree_packed_state_registry.h
@@ -115,9 +115,9 @@ using IStateRegistry = StateRegistry;
 class TreePackedStateRegistry :
     public IStateRegistry {
 
-    valla::IndexedHashSet<valla::Slot<uint32_t>, uint32_t> tree_table;
-    gtl::parallel_flat_hash_map<uint32_t, uint32_t> root_forward;
-    std::vector<uint32_t> root_backward;
+    valla::IndexedHashSet<valla::Slot<PackedStateBin>, PackedStateBin> tree_table;
+    gtl::parallel_flat_hash_map<PackedStateBin, PackedStateBin> root_forward;
+    std::vector<PackedStateBin> root_backward;
 
     const int_packer::IntPacker &state_packer;
     AxiomEvaluator &axiom_evaluator;
